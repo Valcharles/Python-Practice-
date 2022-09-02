714e853f8422905fb460b5ccadf9657ea37a058b
Valentine Ogbodo <valentineogbodo@gmail.com>
2022-09-02 12:06:12 +0100

    Create multiples-of-4.py

---
+++ Odd Or Even/multiples-of-4.py
@@ -0,0 +1,8 @@
+#Get Data from User 
+num= int(input('Enter a number'))
+
+#Proceess and display Results 
+if num%4 == 0:
+    print(str(num) + ' is a multiple of 4')
+else:
+    print(str(num) + ' is not a multiple of 4')
