19a6f1233ede334c5667af5e40b3ae7c31f1283c
Valentine Ogbodo <valentineogbodo@gmail.com>
2022-09-02 12:03:18 +0100

    Create odd-even-checker.py

---
+++ Odd Or Even/odd-even-checker.py
@@ -0,0 +1,10 @@
+#Get data from user
+num= int(input('Enter a number'))
+
+#Process data & Display Output
+if num%2 == 0:
+    print(str(num) + ' is an even number')
+else:
+    print(str(num) + ' is an odd number')
+    
+
