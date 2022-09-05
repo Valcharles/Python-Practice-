ff3d9ba50f4ebcee522d7ac6fd2626518bfdf50f
Valentine Ogbodo <valentineogbodo@gmail.com>
2022-09-05 09:39:30 +0100

    Update is_prime.py

--- is_prime/is_prime.py
+++ is_prime/is_prime.py
@@ -1,6 +1,5 @@
 def is_prime(num):
     factors=[]
-    # num=int(input('Enter number to check if it is a prime number'))
     for i in range(2,num):
         if num%i == 0:
             factors.append(i)
@@ -8,4 +7,4 @@ def is_prime(num):
         print('{} is a prime number'.format(num))
     else:
         print('{} is not a prime number'.format(num))
-        print('{} are some factors of {}'.format(factors,num))
+        print('Some factors of {} :{}'.format(num,factors))
